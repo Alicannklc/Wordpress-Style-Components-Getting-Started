2be67559653983982daf297cb6e1df962988a2dd
alicannklc <alicannklc@gmail.com>
2019-03-02 14:56:45 +0300

    upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <base href="/">
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css">
+    <!-- Builded CSS -->
+    <title>Demo</title>
+</head>
+<body>
+<div id="root"></div>
+
+<!-- Import React Application -->
+<script src="<?php echo get_template_directory_uri() ?>/js/app.js"></script>
+</body>
+</html>
